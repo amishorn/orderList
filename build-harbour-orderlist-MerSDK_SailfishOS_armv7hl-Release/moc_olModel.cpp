02e510c94932c7b2df9967652c78f2a36e84bb64
Ramon Christen <ramon.christen@ch.abb.com>
2016-12-21 15:52:20 +0100

    release_0-2-1

--- build-harbour-orderlist-MerSDK_SailfishOS_armv7hl-Release/moc_olModel.cpp
+++ build-harbour-orderlist-MerSDK_SailfishOS_armv7hl-Release/moc_olModel.cpp
@@ -19,8 +19,8 @@
 
 QT_BEGIN_MOC_NAMESPACE
 struct qt_meta_stringdata_OLModel_t {
-    QByteArrayData data[30];
-    char stringdata[306];
+    QByteArrayData data[31];
+    char stringdata[318];
 };
 #define QT_MOC_LITERAL(idx, ofs, len) \
     Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
@@ -33,42 +33,43 @@ QT_MOC_LITERAL(0, 0, 7),
 QT_MOC_LITERAL(1, 8, 12),
 QT_MOC_LITERAL(2, 21, 0),
 QT_MOC_LITERAL(3, 22, 14),
-QT_MOC_LITERAL(4, 37, 10),
-QT_MOC_LITERAL(5, 48, 10),
-QT_MOC_LITERAL(6, 59, 14),
-QT_MOC_LITERAL(7, 74, 10),
-QT_MOC_LITERAL(8, 85, 6),
-QT_MOC_LITERAL(9, 92, 7),
-QT_MOC_LITERAL(10, 100, 4),
-QT_MOC_LITERAL(11, 105, 8),
-QT_MOC_LITERAL(12, 114, 6),
-QT_MOC_LITERAL(13, 121, 8),
-QT_MOC_LITERAL(14, 130, 4),
-QT_MOC_LITERAL(15, 135, 11),
-QT_MOC_LITERAL(16, 147, 5),
-QT_MOC_LITERAL(17, 153, 10),
-QT_MOC_LITERAL(18, 164, 10),
-QT_MOC_LITERAL(19, 175, 12),
-QT_MOC_LITERAL(20, 188, 13),
-QT_MOC_LITERAL(21, 202, 15),
-QT_MOC_LITERAL(22, 218, 9),
-QT_MOC_LITERAL(23, 228, 11),
+QT_MOC_LITERAL(4, 37, 11),
+QT_MOC_LITERAL(5, 49, 5),
+QT_MOC_LITERAL(6, 55, 10),
+QT_MOC_LITERAL(7, 66, 10),
+QT_MOC_LITERAL(8, 77, 14),
+QT_MOC_LITERAL(9, 92, 10),
+QT_MOC_LITERAL(10, 103, 6),
+QT_MOC_LITERAL(11, 110, 7),
+QT_MOC_LITERAL(12, 118, 4),
+QT_MOC_LITERAL(13, 123, 8),
+QT_MOC_LITERAL(14, 132, 6),
+QT_MOC_LITERAL(15, 139, 8),
+QT_MOC_LITERAL(16, 148, 4),
+QT_MOC_LITERAL(17, 153, 11),
+QT_MOC_LITERAL(18, 165, 10),
+QT_MOC_LITERAL(19, 176, 10),
+QT_MOC_LITERAL(20, 187, 12),
+QT_MOC_LITERAL(21, 200, 13),
+QT_MOC_LITERAL(22, 214, 15),
+QT_MOC_LITERAL(23, 230, 9),
 QT_MOC_LITERAL(24, 240, 11),
 QT_MOC_LITERAL(25, 252, 11),
-QT_MOC_LITERAL(26, 264, 13),
-QT_MOC_LITERAL(27, 278, 4),
-QT_MOC_LITERAL(28, 283, 15),
-QT_MOC_LITERAL(29, 299, 5)
+QT_MOC_LITERAL(26, 264, 11),
+QT_MOC_LITERAL(27, 276, 13),
+QT_MOC_LITERAL(28, 290, 4),
+QT_MOC_LITERAL(29, 295, 15),
+QT_MOC_LITERAL(30, 311, 5)
     },
     "OLModel\0countChanged\0\0sectionChanged\0"
-    "importList\0exportList\0clearSelection\0"
-    "addNewItem\0folded\0section\0name\0selected\0"
-    "entity\0quantity\0note\0replaceItem\0index\0"
-    "removeItem\0moveItemUp\0moveItemDown\0"
-    "moveSectionUp\0moveSectionDown\0setFolded\0"
-    "setQuantity\0getSections\0getEntities\0"
-    "getRoleOfItem\0role\0getOrderListTxt\0"
-    "count\0"
+    "updateEntry\0index\0importList\0exportList\0"
+    "clearSelection\0addNewItem\0folded\0"
+    "section\0name\0selected\0entity\0quantity\0"
+    "note\0replaceItem\0removeItem\0moveItemUp\0"
+    "moveItemDown\0moveSectionUp\0moveSectionDown\0"
+    "setFolded\0setQuantity\0getSections\0"
+    "getEntities\0getRoleOfItem\0role\0"
+    "getOrderListTxt\0count\0"
 };
 #undef QT_MOC_LITERAL
 
@@ -78,59 +79,61 @@ static const uint qt_meta_data_OLModel[] = {
        7,       // revision
        0,       // classname
        0,    0, // classinfo
-      18,   14, // methods
-       1,  174, // properties
+      19,   14, // methods
+       1,  182, // properties
        0,    0, // enums/sets
        0,    0, // constructors
        0,       // flags
        2,       // signalCount
 
  // signals: name, argc, parameters, tag, flags
-       1,    0,  104,    2, 0x06,
-       3,    0,  105,    2, 0x06,
+       1,    0,  109,    2, 0x06,
+       3,    0,  110,    2, 0x06,
 
  // methods: name, argc, parameters, tag, flags
-       4,    0,  106,    2, 0x02,
-       5,    0,  107,    2, 0x02,
-       6,    0,  108,    2, 0x02,
-       7,    7,  109,    2, 0x02,
-      15,    8,  124,    2, 0x02,
-      17,    1,  141,    2, 0x02,
-      18,    1,  144,    2, 0x02,
-      19,    1,  147,    2, 0x02,
-      20,    1,  150,    2, 0x02,
-      21,    1,  153,    2, 0x02,
-      22,    2,  156,    2, 0x02,
-      23,    2,  161,    2, 0x02,
-      24,    0,  166,    2, 0x02,
-      25,    0,  167,    2, 0x02,
-      26,    2,  168,    2, 0x02,
-      28,    0,  173,    2, 0x02,
+       4,    1,  111,    2, 0x02,
+       6,    0,  114,    2, 0x02,
+       7,    0,  115,    2, 0x02,
+       8,    0,  116,    2, 0x02,
+       9,    7,  117,    2, 0x02,
+      17,    8,  132,    2, 0x02,
+      18,    1,  149,    2, 0x02,
+      19,    1,  152,    2, 0x02,
+      20,    1,  155,    2, 0x02,
+      21,    1,  158,    2, 0x02,
+      22,    1,  161,    2, 0x02,
+      23,    2,  164,    2, 0x02,
+      24,    2,  169,    2, 0x02,
+      25,    0,  174,    2, 0x02,
+      26,    0,  175,    2, 0x02,
+      27,    2,  176,    2, 0x02,
+      29,    0,  181,    2, 0x02,
 
  // signals: parameters
     QMetaType::Void,
     QMetaType::Void,
 
  // methods: parameters
+    QMetaType::Void, QMetaType::Int,    5,
     QMetaType::Bool,
     QMetaType::Bool,
     QMetaType::Void,
-    QMetaType::Void, QMetaType::Bool, QMetaType::QString, QMetaType::QString, QMetaType::Bool, QMetaType::QString, QMetaType::QString, QMetaType::QString,    8,    9,   10,   11,   12,   13,   14,
-    QMetaType::Void, QMetaType::Int, QMetaType::Bool, QMetaType::QString, QMetaType::QString, QMetaType::Bool, QMetaType::QString, QMetaType::QString, QMetaType::QString,   16,    8,    9,   10,   11,   12,   13,   14,
-    QMetaType::Void, QMetaType::Int,   16,
-    QMetaType::Void, QMetaType::Int,   16,
-    QMetaType::Void, QMetaType::Int,   16,
-    QMetaType::Void, QMetaType::QString,    9,
-    QMetaType::Void, QMetaType::QString,    9,
-    QMetaType::Void, QMetaType::Int, QMetaType::Bool,   16,    8,
-    QMetaType::Void, QMetaType::Int, QMetaType::QString,   16,   13,
+    QMetaType::Void, QMetaType::Bool, QMetaType::QString, QMetaType::QString, QMetaType::Bool, QMetaType::QString, QMetaType::QString, QMetaType::QString,   10,   11,   12,   13,   14,   15,   16,
+    QMetaType::Void, QMetaType::Int, QMetaType::Bool, QMetaType::QString, QMetaType::QString, QMetaType::Bool, QMetaType::QString, QMetaType::QString, QMetaType::QString,    5,   10,   11,   12,   13,   14,   15,   16,
+    QMetaType::Void, QMetaType::Int,    5,
+    QMetaType::Void, QMetaType::Int,    5,
+    QMetaType::Void, QMetaType::Int,    5,
+    QMetaType::Void, QMetaType::QString,   11,
+    QMetaType::Void, QMetaType::QString,   11,
+    QMetaType::Void, QMetaType::Int, QMetaType::Bool,    5,   10,
+    QMetaType::Void, QMetaType::Int, QMetaType::QString,    5,   15,
     QMetaType::QVariant,
     QMetaType::QVariant,
-    QMetaType::QVariant, QMetaType::Int, QMetaType::QString,   16,   27,
+    QMetaType::QVariant, QMetaType::Int, QMetaType::QString,    5,   28,
     QMetaType::QString,
 
  // properties: name, type, flags
-      29, QMetaType::Int, 0x00495001,
+      30, QMetaType::Int, 0x00495001,
 
  // properties: notify_signal_id
        0,
@@ -145,27 +148,28 @@ void OLModel::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, voi
         switch (_id) {
         case 0: _t->countChanged(); break;
         case 1: _t->sectionChanged(); break;
-        case 2: { bool _r = _t->importList();
+        case 2: _t->updateEntry((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 3: { bool _r = _t->importList();
             if (_a[0]) *reinterpret_cast< bool*>(_a[0]) = _r; }  break;
-        case 3: { bool _r = _t->exportList();
+        case 4: { bool _r = _t->exportList();
             if (_a[0]) *reinterpret_cast< bool*>(_a[0]) = _r; }  break;
-        case 4: _t->clearSelection(); break;
-        case 5: _t->addNewItem((*reinterpret_cast< bool(*)>(_a[1])),(*reinterpret_cast< QString(*)>(_a[2])),(*reinterpret_cast< QString(*)>(_a[3])),(*reinterpret_cast< bool(*)>(_a[4])),(*reinterpret_cast< QString(*)>(_a[5])),(*reinterpret_cast< QString(*)>(_a[6])),(*reinterpret_cast< QString(*)>(_a[7]))); break;
-        case 6: _t->replaceItem((*reinterpret_cast< int(*)>(_a[1])),(*reinterpret_cast< bool(*)>(_a[2])),(*reinterpret_cast< QString(*)>(_a[3])),(*reinterpret_cast< QString(*)>(_a[4])),(*reinterpret_cast< bool(*)>(_a[5])),(*reinterpret_cast< QString(*)>(_a[6])),(*reinterpret_cast< QString(*)>(_a[7])),(*reinterpret_cast< QString(*)>(_a[8]))); break;
-        case 7: _t->removeItem((*reinterpret_cast< int(*)>(_a[1]))); break;
-        case 8: _t->moveItemUp((*reinterpret_cast< int(*)>(_a[1]))); break;
-        case 9: _t->moveItemDown((*reinterpret_cast< int(*)>(_a[1]))); break;
-        case 10: _t->moveSectionUp((*reinterpret_cast< QString(*)>(_a[1]))); break;
-        case 11: _t->moveSectionDown((*reinterpret_cast< QString(*)>(_a[1]))); break;
-        case 12: _t->setFolded((*reinterpret_cast< int(*)>(_a[1])),(*reinterpret_cast< bool(*)>(_a[2]))); break;
-        case 13: _t->setQuantity((*reinterpret_cast< int(*)>(_a[1])),(*reinterpret_cast< QString(*)>(_a[2]))); break;
-        case 14: { QVariant _r = _t->getSections();
+        case 5: _t->clearSelection(); break;
+        case 6: _t->addNewItem((*reinterpret_cast< bool(*)>(_a[1])),(*reinterpret_cast< QString(*)>(_a[2])),(*reinterpret_cast< QString(*)>(_a[3])),(*reinterpret_cast< bool(*)>(_a[4])),(*reinterpret_cast< QString(*)>(_a[5])),(*reinterpret_cast< QString(*)>(_a[6])),(*reinterpret_cast< QString(*)>(_a[7]))); break;
+        case 7: _t->replaceItem((*reinterpret_cast< int(*)>(_a[1])),(*reinterpret_cast< bool(*)>(_a[2])),(*reinterpret_cast< QString(*)>(_a[3])),(*reinterpret_cast< QString(*)>(_a[4])),(*reinterpret_cast< bool(*)>(_a[5])),(*reinterpret_cast< QString(*)>(_a[6])),(*reinterpret_cast< QString(*)>(_a[7])),(*reinterpret_cast< QString(*)>(_a[8]))); break;
+        case 8: _t->removeItem((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 9: _t->moveItemUp((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 10: _t->moveItemDown((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 11: _t->moveSectionUp((*reinterpret_cast< QString(*)>(_a[1]))); break;
+        case 12: _t->moveSectionDown((*reinterpret_cast< QString(*)>(_a[1]))); break;
+        case 13: _t->setFolded((*reinterpret_cast< int(*)>(_a[1])),(*reinterpret_cast< bool(*)>(_a[2]))); break;
+        case 14: _t->setQuantity((*reinterpret_cast< int(*)>(_a[1])),(*reinterpret_cast< QString(*)>(_a[2]))); break;
+        case 15: { QVariant _r = _t->getSections();
             if (_a[0]) *reinterpret_cast< QVariant*>(_a[0]) = _r; }  break;
-        case 15: { QVariant _r = _t->getEntities();
+        case 16: { QVariant _r = _t->getEntities();
             if (_a[0]) *reinterpret_cast< QVariant*>(_a[0]) = _r; }  break;
-        case 16: { QVariant _r = _t->getRoleOfItem((*reinterpret_cast< int(*)>(_a[1])),(*reinterpret_cast< QString(*)>(_a[2])));
+        case 17: { QVariant _r = _t->getRoleOfItem((*reinterpret_cast< int(*)>(_a[1])),(*reinterpret_cast< QString(*)>(_a[2])));
             if (_a[0]) *reinterpret_cast< QVariant*>(_a[0]) = _r; }  break;
-        case 17: { QString _r = _t->getOrderListTxt();
+        case 18: { QString _r = _t->getOrderListTxt();
             if (_a[0]) *reinterpret_cast< QString*>(_a[0]) = _r; }  break;
         default: ;
         }
@@ -212,13 +216,13 @@ int OLModel::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     if (_id < 0)
         return _id;
     if (_c == QMetaObject::InvokeMetaMethod) {
-        if (_id < 18)
+        if (_id < 19)
             qt_static_metacall(this, _c, _id, _a);
-        _id -= 18;
+        _id -= 19;
     } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
-        if (_id < 18)
+        if (_id < 19)
             *reinterpret_cast<int*>(_a[0]) = -1;
-        _id -= 18;
+        _id -= 19;
     }
 #ifndef QT_NO_PROPERTIES
       else if (_c == QMetaObject::ReadProperty) {
